7b3622cf5222b596a74d55bd14ca2180e01b3a3e
Phat <damphat@gmail.com>
2020-07-12 19:06:02 +0700

    add CalcTests

--- Calculator.Tests/CalcTests.cs
+++ Calculator.Tests/CalcTests.cs
@@ -5,8 +5,36 @@ namespace Calculator.Tests {
     [TestClass]
     public class CalcTests {
         [TestMethod]
-        public void It_works() {
-            Assert.AreEqual(-9, Calc.Eval("-(1 + 2) * 3"));
+        public void Unary() {
+            Assert.AreEqual(+1, Calc.Eval("+1"));
+            Assert.AreEqual(-1, Calc.Eval("-1"));
+        }
+
+        [TestMethod]
+        public void Binary() {
+            Assert.AreEqual(+5, Calc.Eval("2+3"));
+            Assert.AreEqual(-1, Calc.Eval("2-3"));
+            Assert.AreEqual(+6, Calc.Eval("2*3"));
+            Assert.AreEqual(2.0/3, Calc.Eval("2/3"));
+        }
+
+        [TestMethod]
+        public void Parentheses() {
+            Assert.AreEqual(2, Calc.Eval("(((1)+(1)))"));
+        }
+
+        [TestMethod]
+        public void Associativity() {
+            Assert.AreEqual(1 - 1 - 1, Calc.Eval("1-1-1"));
+        }
+
+        [TestMethod]
+        public void Precedence() {
+            Assert.AreEqual(1 + 2 * 3, Calc.Eval("1+2*3"));
+            Assert.AreEqual((1 + 2) * 3, Calc.Eval("(1+2)*3"));
+            Assert.AreEqual(2 * 3 + 1, Calc.Eval("2*3 + 1"));
+            Assert.AreEqual(2 * (3 + 1), Calc.Eval("2*(3 + 1)"));
+
         }
     }
 }
