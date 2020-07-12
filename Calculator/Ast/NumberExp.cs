ed82bc337fa9bfabe4ff093cf73dded0821aa7ab
Phat <damphat@gmail.com>
2020-07-13 00:32:31 +0700

    big numbers, such as 1E+1000, turn to Infinity instead of NaN

--- Calculator/Ast/NumberExp.cs
+++ Calculator/Ast/NumberExp.cs
@@ -1,4 +1,8 @@
-﻿namespace Calculator.Ast {
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Calculator.Ast {
     public class NumberExp : Exp {
         public readonly Token Number;
 
@@ -8,7 +12,17 @@
         }
 
         protected override double Eval() {
-            return double.TryParse(Number.Raw, out var ret) ? ret : double.NaN;
+            try {
+                return double.Parse(Number.Raw, CultureInfo.InvariantCulture);
+            } catch (OverflowException) {
+                if (Regex.IsMatch(Number.Raw, "^\\S-")) {
+                    return double.NegativeInfinity;
+                } else {
+                    return double.PositiveInfinity;
+                }
+            } catch (FormatException) {
+                return double.NaN;
+            }
         }
 
         public override string ToString() {
