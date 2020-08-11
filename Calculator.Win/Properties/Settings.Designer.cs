e421de5d751b8d322aae1cfc2ab2db1d98303553
Phat <damphat@gmail.com>
2020-08-11 21:13:49 +0700

    [learn] Try WinForms with .NET Core

--- Calculator.Win/Properties/Settings.Designer.cs
+++ Calculator.Win/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Calculator.Win.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace Calculator.Win.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("(1 + 2) * 3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("- (1 + 2) * 3")]
         public string Src {
             get {
                 return ((string)(this["Src"]));
